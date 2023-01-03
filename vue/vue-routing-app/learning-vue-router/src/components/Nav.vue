a4b9cbcd5b126ff81724156681750ce79feb71f3
Gaurav Pawar <gauravpawar3102@gmail.com>
2023-01-03 23:20:57 +0530

    started routing

---
+++ vue/vue-routing-app/learning-vue-router/src/components/Nav.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="container">
+    <RouterLink id="nav-item" active-class="active" to="/">Home</RouterLink>
+    <RouterLink id="nav-item" active-class="active" to="/about"
+      >About</RouterLink
+    >
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+}
+#nav-item {
+  text-decoration: none;
+}
+.active {
+  font-weight: bold;
+  color: brown;
+}
+</style>
